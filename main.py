84bf6561e097c30093d67a8ceed1025f246a0ded
ALPER AYDIN <info@alperaydin.net>
2020-08-27 01:41:11 +0300

    deneme

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def init():
+    return 'module initialized'
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=8080, debug=True)
